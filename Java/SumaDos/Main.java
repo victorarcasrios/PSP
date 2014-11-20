1a0fd95ba8b79743a27bc50bdc1533dd44622346
victorarcasrios <victorarcasrios@gmail.com>
2014-11-20 08:50:02 +0100

    Implemented main functionality

--- Java/SumaDos/Main.java
+++ Java/SumaDos/Main.java
@@ -3,18 +3,28 @@ import java.io.*;
 public class Main{
 	public static void main(String[]args){
 
-		String command = "java Adder";
-		String input = "10\n";
+		final String COMMAND = "java Adder";
+		final String INPUT = "10\n";
+		String line;
 
 		Runtime runtime = Runtime.getRuntime();
 		Process process;
 
 		try{
-			process = runtime.exec(command);
+			process = runtime.exec(COMMAND);
 
-			// OutputStream os = process.getOutputStream();
-			// os.write(input.getBytes());
-			// os.flush();
+			OutputStream os = process.getOutputStream();
+			for(int i = 0; i < 2; i++){
+				os.write(INPUT.getBytes());
+				os.flush();
+			}
+
+			InputStream is = process.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+			while((line = br.readLine()) != null)
+				System.out.println(line);
+			br.close();
 		}catch(IOException ioex){
 			ioex.printStackTrace();
 		}
